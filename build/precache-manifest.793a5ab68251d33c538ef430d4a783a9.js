e62e4f7982730031ea70ecab34d7aee76dfbc558
Chris <78775458+ChrisThinkful@users.noreply.github.com>
2021-06-06 23:26:07 -0500

    vercel changes

---
+++ build/precache-manifest.793a5ab68251d33c538ef430d4a783a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e38d41722c97d7d8d21f84abef76331",
+    "url": "/index.html"
+  },
+  {
+    "revision": "529d71f3e2f6f2301107",
+    "url": "/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "226820f6af32e486a61f",
+    "url": "/static/js/2.0885f580.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0885f580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "529d71f3e2f6f2301107",
+    "url": "/static/js/main.f0478da5.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
